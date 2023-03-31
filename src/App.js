d81a14fc563d933160aa4d4a3e0f1b5b0354345e
Dhruvin Sorathiya <dhruvinsorathiya102.15@gmail.com>
2023-03-31 12:14:35 +0530

    complte puzzle

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-import "./App.css"
-import Calculator from './Component/Calculator'
-import TicTacToe from './Component/TicTacToe';
-import Puzzle from './Component/Puzzle';
+import "./App.css";
+import Calculator from "./Component/Calculator";
+import TicTacToe from "./Component/TicTacToe";
+import Puzzle from "./Component/Puzzle";
+import Table from "./Table";
 
 const App = () => {
-
   return (
     <>
       <div className='bg-dark p-3'>
-        <Link to="/" className='text-white btn btn-outline-warning mx-2'>Calculator</Link>
+        <Link to="/calc" className='text-white btn btn-outline-warning mx-2'>Calculator</Link>
         <Link to="tictactoe" className='text-white btn btn-outline-warning mx-2'>TicTacToe</Link>
         <Link to="puzzle" className='text-white btn btn-outline-warning mx-2'>Puzzle</Link>
       </div>
+
+      {/* <Table data={data} /> */}
       <div className="">
         <Routes>
-          <Route path="/" element={<Calculator />} />
+          <Route path="calc" element={<Calculator />} />
           <Route path="tictactoe" element={<TicTacToe />} />
           <Route path="puzzle" element={<Puzzle />} />
+          <Route path="table" element={<Table />} />
         </Routes>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
---
+++ src/Table.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Table = (props) => {
+  return (
+    <div className='text-light'>Table</div>
+  )
+}
+
+export default Table;
